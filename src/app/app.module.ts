ef0c4de602e6f3d552c6ad5709a3caa713f18535
levi7win <ed2leon@hotmail.com>
2018-03-02 16:46:06 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRouterModule } from './app.routes';
+
+import { PageModule } from './pages/page.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-
-import { PageComponent } from './pages/page.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './login/register.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
     LoginComponent,
-    DashboardComponent,
-    PageComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRouterModule,
+    PageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
